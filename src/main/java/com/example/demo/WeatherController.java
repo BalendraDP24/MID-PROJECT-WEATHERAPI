717ad008ff2daf92989721a4863317841bb0a940
Balendra Dp <balendra.dp@publicissapient.com>
2023-10-06 10:15:04 +0530

    first commit

---
+++ src/main/java/com/example/demo/WeatherController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/weather")
+public class WeatherController {
+
+	private final WeatherService weatherService;
+
+	@Autowired
+	public WeatherController(WeatherService weatherService) {
+		this.weatherService = weatherService;
+	}
+
+	@GetMapping("/current/{location}")
+	public String getCurrentWeather(@PathVariable String location) {
+		weatherService.fetchAndSaveWeatherData(location);
+		return "Weather data for location " + location + " fetched and saved.";
+	}
+}
